77bd47e99d4aee995e26ff9ce716660bb15f15a5
dot-ruth <ruthgetaneh5@gmail.com>
2023-06-06 15:08:48 +0300

    added the online IDE Functionality

---
+++ dot-lms/src/Components/CodeEditorWindow.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import { Box } from "@mui/material";
+import { Editor } from "@monaco-editor/react";
+export default function CodeEditorWindow ({onchange,language,code,theme}){
+    const [value,setValue] = useState(code );
+
+    const handleEditorChange = (value) => {
+        setValue(value);
+        onchange("code",value);
+    }
+
+    return (
+        <Box className="overlay rounded-md w-full h-full shadow-4xl">
+      <Editor
+        height="100vh"
+        width="100vh"
+        language={language || "javascript"}
+        value={value}
+        theme={theme}
+        defaultValue="// some comment"
+        onChange={handleEditorChange}
+      />
+    </Box>
+  );
+  
+    
+}
+
