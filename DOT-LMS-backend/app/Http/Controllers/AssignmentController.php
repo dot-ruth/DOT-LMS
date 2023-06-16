952619ad14c4b74eb5a6a1eb81bc7f059d251b38
dot-ruth <ruthgetaneh5@gmail.com>
2023-06-16 15:51:42 +0300

    updated the API endpoint

---
+++ DOT-LMS-backend/app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\assignment;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AssignmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(assignment $assignment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, assignment $assignment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(assignment $assignment)
+    {
+        //
+    }
+}
---
+++ DOT-LMS-backend/database/migrations/2023_06_16_123611_create_assignments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->string('assignment_id')->unique();
+            $table->string('assignment_title');
+            $table->string('assignment_content');
+            $table->foreignId('course_id');
+            $table->foreignId('student_id');
+            $table->foreignId('teacher_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
